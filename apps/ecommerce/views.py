6efb7033b33f57aad4fa1a8b5b9dfb2cb924cb8a
reyes-27 <dani27_arr@hotmail.com>
2024-02-20 22:26:26 -0300

    add-CRUD-products

--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from apps.items.models import Product
 from .serializers import ProductSerializer
+from django.http import Http404
 # Create your views here.
 
 class ProductListAPIView(APIView):
@@ -15,9 +16,34 @@ class ProductListAPIView(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True, read_only=True, context={"request":request})
         return Response(data={"data":serializer.data}, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 class ProductDetailAPIView(APIView):
     permission_classes=[AllowAny, ]
+    def get_object(self, slug:str):
+        try:
+            obj = Product.objects.get(slug=slug)
+            return obj
+        except:
+            raise Http404("Product does not exist.")
+        
     def get(self, request, format=None, *args, **kwargs):
-        product = Product.objects.get(slug=kwargs["slug"])
+        product = self.get_object(kwargs["slug"])
         serializer = ProductSerializer(instance=product, context={"request":request})
         return Response(data={"data":serializer.data}, status=status.HTTP_200_OK)
+    
+    def patch(self, request, format=None, *args, **kwargs):
+        product = self.get_object(kwargs["slug"])
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data={"product":serializer.data})
+        else:
+            return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
