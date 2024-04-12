5bd94899736e9335290f0703030cf3eb35f12c79
reyes-27 <dani27_arr@hotmail.com>
2024-04-12 10:26:38 -0400

    tasks module created

--- my_celery_worker/my_celery.py
+++ my_celery_worker/my_celery.py
@@ -7,6 +7,5 @@ from celery import Celery
 
 app = Celery("tasks")
 app.config_from_object('my_celery_config')
-# app.conf.broker_url = 'redis://redis:6379/0'
-# app.conf.result_backend = 'redis://redis:6379/0'
+
 # app.autodiscover_tasks()
