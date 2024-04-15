150358a3af75ae33188b366b7a547f95d099e185
reyes-27 <dani27_arr@hotmail.com>
2024-04-15 09:14:20 -0400

    celery_settings

--- my_celery_worker/my_celery.py
+++ my_celery_worker/my_celery.py
@@ -6,18 +6,17 @@ from celery import Celery
 # env.read_env(os.path.join(BASE_DIR,'../core/.env'))
 
 app = Celery("tasks",
-            broker="redis://redis:6379/0",
-            backend="redis://redis:6379/0",
+            # broker="redis://redis:6379/0",
+            # backend="redis://redis:6379/0",
             include=[
                 'tasks.ecommerce_tasks',
                     ],
             )
-# app.config_from_object('my_celery_config')
+app.config_from_object('celery_settings')
 app.conf.update(
     result_expires=3600,
 )
-# p = ['tasks.ecommerce_tasks',]
-# app.autodiscover_tasks(ecit()p , force=True)
+
 app.autodiscover_tasks()
 
 app.task()
