f90375d835f45d2c015223f07e0e324b53d8e6f3
reyes-27 <dani27_arr@hotmail.com>
2024-07-07 00:50:33 -0400

    fix

--- apps/invoices/models.py
+++ apps/invoices/models.py
@@ -3,6 +3,5 @@ from apps.orders.models import Order
 # Create your models here.
 
 class Invoice(models.Model):
-    order =         models.ForeignKey(Order, related_name="invoices", null=True, blank=True)
+    order =         models.ForeignKey(Order, related_name="invoices", null=True, blank=True, on_delete=models.CASCADE)
     comment =       models.TextField()
-    
